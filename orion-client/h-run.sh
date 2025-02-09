6d13f869494b539d405e6c964765fdb70ee3efb2
Xefier <admin@xefiergames.com>
2025-02-09 17:24:22 -0600

    Implemented OrionClient Hive Custom Miner

---
+++ orion-client/h-run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Load h-manifest configuration
+if [[ ! -f h-manifest.conf ]]; then
+    echo "h-manifest.conf not found. Exiting..."
+    exit 1
+fi
+
+. h-manifest.conf
+
+if [[ ! -f $CUSTOM_CONFIG_FILENAME ]]; then
+    echo "Configuration file $CUSTOM_CONFIG_FILENAME not found. Exiting..."
+    exit 1
+fi
+
+. "$CUSTOM_CONFIG_FILENAME"
+
+# TODO: Configure gpu, cpu, block size, etc in flightsheet
+./OrionClient mine --key "$TEMPLATE" --gpu --disable-cpu --gpu-batch-size 2048 --gpu-block-size 512
