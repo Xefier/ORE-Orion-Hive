61a7149ef1a902dcc08ec75a5ce44771a24c0c56
Xefier <admin@xefiergames.com>
2025-02-09 21:34:24 -0600

    Fixed algorithm in h-stats.sh again

--- orion-client/h-stats.sh
+++ orion-client/h-stats.sh
@@ -13,5 +13,5 @@ ver="1.0.0"
 bus_numbers=[null]
 
 khs=0.001
-stats=$(jq -n --argjson uptime "$uptime" --argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" --arg algo "COAL/ORE" --arg ver "$ver" --argjson bus_numbers "$bus_numbers" '{$hs, $temp, $fan, $uptime, $algo, $ver, $bus_numbers}')
+stats=$(jq -n --argjson uptime "$uptime" --argjson hs "$hs" --argjson temp "$temp" --argjson fan "$fan" --arg algo "$CUSTOM_ALGO" --arg ver "$ver" --argjson bus_numbers "$bus_numbers" '{$hs, $temp, $fan, $uptime, $algo, $ver, $bus_numbers}')
 
